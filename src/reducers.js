76ba84db9a30e56b7fd1571882aaa3850f12eb2c
milesswu <mileswu13@gmail.com>
2019-09-12 13:15:48 -0700

    Add runStatus data
    Create reducer to keep track of whether the array is currently being
    sorted. This state data will be used to disable interactivity with the
    options bar to prevent changing state while a sorting algorithm
    executes.

--- src/reducers.js
+++ src/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import array from "./app/reducers/array";
 import algorithm from "./app/reducers/algorithm";
 import elements from "./app/reducers/element-types";
+import runStatus from "./app/reducers/run-status";
 
 const rootReducer = combineReducers({
-	array,
 	algorithm,
+	array,
 	elements,
+	runStatus,
 });
 
 export default rootReducer;
